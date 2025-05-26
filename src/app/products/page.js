7cb073de3d9720d9d9a402143c40c387bf97ca5b
adiadishr <146527912+adiadishr@users.noreply.github.com>
2025-05-26 15:38:41 +0545

    fix:products cols change

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -17,7 +17,7 @@ export default function Page() {
       </div>
       <div className="col-span-1 md:col-span-3">
         <div className="mb-8 text-3xl">Award Winning Diapers</div>
-        <div className="grid grid-cols-2 gap-x-8 gap-y-12">
+        <div className="grid grid-cols-3 gap-x-8 gap-y-12">
           {Array.from({ length: 5 }).map((_, index) => (
             <div className="flex flex-col" key={index}>
               <div className="relative mb-4 aspect-square">
@@ -27,7 +27,7 @@ export default function Page() {
                   fill
                   className="object-cover rounded-md"
                 />
-                <div className="absolute z-10 p-2 text-sm rounded-md bg-white/80 text-cyan-950 top-4 left-4">Up to 12 hours of leak protection</div>
+                <div className="absolute z-10 px-2 py-1 text-xs rounded-md bg-white/80 text-cyan-950 top-2 left-2">Up to 12 hours of leak protection</div>
               </div>
               <div className="flex flex-col gap-2 mb-2 md:items-center md:flex-row">
                 <div className="flex gap-2">
