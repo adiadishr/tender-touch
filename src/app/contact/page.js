69e6e8e29fda3bb13146486f435e67bf5dee85c9
adiadishr <146527912+adiadishr@users.noreply.github.com>
2025-05-26 11:54:24 +0545

    embedded map

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -18,10 +18,22 @@ export default function Page() {
         Contact Us
       </div>
     </div>
-    <div className="py-20 w-[90%] mx-auto border-b border-neutral-200 grid md:grid-cols-2 text-cyan-950 relative">
-      <div className="border-r border-neutral-200 md:pr-[10%] sticky top-20">
+    <div className="py-20 w-[90%] mx-auto border-b border-neutral-200 grid md:grid-cols-2 text-cyan-950">
+      <div className="border-b pb-14 mb-10 md:pb-0 md:mb-0 md:border-b-0 md:border-r border-neutral-200 md:pr-[10%]">
         <div className="text-3xl tracking-tight mb-4">We&apos;re here to help</div>
-        <div className="text-base/6 tracking-tight">If you weren&apos;t able to find the answer in our <Link className="inline text-violet-400 underline" href="/#">FAQs</Link>, fill out this form with your details and tell us how we can help. Our friendly Customer Love team will be in touch shortly!</div>
+        <div className="text-base/6 tracking-tight mb-8">
+          If you weren&apos;t able to find the answer in our <Link className="inline text-violet-400 underline" href="/#">FAQs</Link>, fill out this form with your details and tell us how we can help. Our friendly Customer Love team will be in touch shortly!
+        </div>
+        <div className="relative w-full h-64 md:h-80 -z-10">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3533.0297272956777!2d85.34222571040279!3d27.68547607609577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb198d7e8b05e9%3A0x417ff954dc19632f!2sBrajeshwori%20Group!5e0!3m2!1sen!2snp!4v1748239269648!5m2!1sen!2snp"
+            className="w-full h-full"
+            style={{ border: 0 }}
+            allowFullScreen={false}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+        </div>
       </div>
       <form className="md:px-[10%] grid md:grid-cols-2 gap-x-4 gap-y-6 contact-form">
         <div className="">
